8f1ef2851af3060b7acb008940ade982a4c96352
zerrouki djamel <djameljimmizerrouki@gmail.com>
2018-12-26 04:48:33 +0100

    new config

--- submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-$org_name = mysql_escape_string($_POST['org_name']);
-
-$org_name_split = explode('.', $org_name);
-$org_name_full = $org_name_split[0];
-$org_name_key = $org_name_split[1];
-
-$strSQL = "INSERT INTO Database(org_name_full, org_name_key) VALUES ('$org_name_full', '$org_name_key')"; 
-?>
